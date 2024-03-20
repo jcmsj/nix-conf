e1206a1aacb78c16238aca3bd5e0dff59da17bd9
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-03-21 07:18:30 +0800

    remove home-manager from systemPackages

--- configuration.nix
+++ configuration.nix
@@ -56,7 +56,6 @@
     ## Required Apps
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # notif daemon
-    home-manager
     mako
     libnotify
     # Wallpaper
