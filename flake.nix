74760ebf8ff2e2e833604a10e227f1426a0b84d7
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-04-13 09:35:03 +0800

    Add flake: auto-cpufreq

--- flake.nix
+++ flake.nix
@@ -9,60 +9,65 @@
     #   url = "github:Aylur/ags";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
+    auto-cpufreq = {
+      url = "github:AdnanHodzic/auto-cpufreq";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     monitorSwitcher.url = "github:jcmsj/hypr-conf/main";
-  };  
-    outputs = inputs@{ nixpkgs, ... }: 
-    let 
-      username = "jcsan"; 
+  };
+  outputs = inputs@{ nixpkgs, auto-cpufreq, ... }:
+    let
+      username = "jcsan";
       system = "x86_64-linux";
-    legacyPackages = nixpkgs.lib.genAttrs [ system ] (system:
-      import inputs.nixpkgs {
-        inherit system;
-        # NOTE: Using `nixpkgs.config` in your NixOS config won't work
-        # Instead, you should set nixpkgs configs here
-        # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
+      legacyPackages = nixpkgs.lib.genAttrs [ system ] (system:
+        import inputs.nixpkgs {
+          inherit system;
+          # NOTE: Using `nixpkgs.config` in your NixOS config won't work
+          # Instead, you should set nixpkgs configs here
+          # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
 
-        config.allowUnfree = true;
-        config.firefox.speechSynthesisSupport = true;
-      });
-    pkgs = legacyPackages.${system};
+          config.allowUnfree = true;
+          config.firefox.speechSynthesisSupport = true;
+        });
+      pkgs = legacyPackages.${system};
     in
     {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-            specialArgs = {  
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = {
             inherit system;
             inherit pkgs;
             inherit inputs; # will let de.nix be able to use the hyprland flake.
           };
-        modules = [
-          ./configuration.nix
-        # FIX: option `home` does not exist
-        # ags.homeManagerModules.default  # another could be ags
-           #  home-manager.nixosModules.home-manager
-        # {
-         #  home-manager.useGlobalPkgs = true;
-         #   home-manager.useUserPackages = true;
-         #   home-manager.users.${username} = import ./home.nix;
+          modules = [
+            ./configuration.nix
+            auto-cpufreq.nixosModules.default
+            # FIX: option `home` does not exist
+            # ags.homeManagerModules.default  # another could be ags
+            #  home-manager.nixosModules.home-manager
+            # {
+            #  home-manager.useGlobalPkgs = true;
+            #   home-manager.useUserPackages = true;
+            #   home-manager.users.${username} = import ./home.nix;
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
-         # }
-        ];
+            # }
+          ];
+        };
       };
-    };
 
-    # FIX: service doesnt start
-    # home-manager.useGlobalPkgs = true;
-    # home-manager.useUserPackages = true;
-    # homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-    #   inherit pkgs;
-    #   # Specify your home configuration modules here, for example,
-  
-    #   # the path to your home.nix.
-    #   modules = [
-    #     ./home.nix
-    #     ags.homeManagerModules.default  # another could be ags
-    #     ];
-    # };
-  };
+      # FIX: service doesnt start
+      # home-manager.useGlobalPkgs = true;
+      # home-manager.useUserPackages = true;
+      # homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+      #   inherit pkgs;
+      #   # Specify your home configuration modules here, for example,
+
+      #   # the path to your home.nix.
+      #   modules = [
+      #     ./home.nix
+      #     ags.homeManagerModules.default  # another could be ags
+      #     ];
+      # };
+    };
 }
--- power.nix
+++ power.nix
@@ -3,18 +3,17 @@
 {
   # Power management https://wiki.nixos.org/wiki/Laptop
   services.power-profiles-daemon.enable = false;
-  
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-    };
+
+  programs.auto-cpufreq.enable = true;
+  programs.auto-cpufreq.settings = {
     charger = {
       governor = "performance";
       turbo = "auto";
     };
-  };
 
-  # powerManagement.powertop.enable = true;
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+  };
 }
