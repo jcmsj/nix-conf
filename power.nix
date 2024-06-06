bc6ec5405d006dcfe4c969158838a6bdb1f456d5
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-06-06 22:05:50 +0800

    Format: auto-cpufreq

--- power.nix
+++ power.nix
@@ -4,16 +4,18 @@
   # Power management https://wiki.nixos.org/wiki/Laptop
   services.power-profiles-daemon.enable = false;
 
-  programs.auto-cpufreq.enable = true;
-  programs.auto-cpufreq.settings = {
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
+  programs.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
 
-    battery = {
-      governor = "powersave";
-      turbo = "never";
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
     };
   };
 }
